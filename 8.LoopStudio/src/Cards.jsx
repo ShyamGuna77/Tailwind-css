97da031fb536c9f0e5c8ce2feb274c6a139d17f2
Shyam <gshyamprasad2001@gmail.com>
2025-02-03 15:43:09 +0530

    finished

--- 8.LoopStudio/src/Cards.jsx
+++ 8.LoopStudio/src/Cards.jsx
@@ -2,64 +2,132 @@
 
 const Cards = () => {
     return (
-        <div className="mt-16 container max-w-6xl px-6">
-            <div>
-                <div className="flex flex-col justify-between md:flex-row">
-                    <p className="font-mono uppercase font-light text-3xl">Our creations</p>
-                    <button className="border border-black px-8 py-2 text-black bg-white hover:bg-black hover:text-white">SEE ALL</button>
-                </div>
-            </div>
-            <div className="grid gap-6 md:grid-cols-4 mt-8">
-                <div className="group item">
-                    <img src="images/desktop/image-deep-earth.jpg" alt="" className="hidden w-full duration-200 md:block group-hover:scale-110" />
-                    <img src="images/mobile/image-deep-earth.jpg" alt="" className="w-full md:hidden" />
-                    <div className="item-gradient"></div>
-                    <h5>Deep Earth</h5>
-                </div>
-                <div className="group item">
-                    <img src="images/desktop/image-night-arcade.jpg" alt="" className="hidden w-full duration-200 md:block group-hover:scale-110" />
-                    <img src="images/mobile/image-night-arcade.jpg" alt="" className="w-full md:hidden" />
-                    <div className="item-gradient"></div>
-                    <h5>Night Arcade</h5>
-                </div>
-                <div className="group item">
-                    <img src="images/desktop/image-soccer-team.jpg" alt="" className="hidden w-full duration-200 md:block group-hover:scale-110" />
-                    <img src="images/mobile/image-soccer-team.jpg" alt="" className="w-full md:hidden" />
-                    <div className="item-gradient"></div>
-                    <h5>Soccer Team VR</h5>
-                </div>
-                <div className="group item">
-                    <img src="images/desktop/image-grid.jpg" alt="" className="hidden w-full duration-200 md:block group-hover:scale-110" />
-                    <img src="images/mobile/image-grid.jpg" alt="" className="w-full md:hidden" />
-                    <div className="item-gradient"></div>
-                    <h5>The Grid</h5>
-                </div>
-                <div className="group item">
-                    <img src="images/desktop/image-from-above.jpg" alt="" className="hidden w-full duration-200 md:block group-hover:scale-110" />
-                    <img src="images/mobile/image-from-above.jpg" alt="" className="w-full md:hidden" />
-                    <div className="item-gradient"></div>
-                    <h5>From Up Above VR</h5>
-                </div>
-                <div className="group item">
-                    <img src="images/desktop/image-pocket-borealis.jpg" alt="" className="hidden w-full duration-200 md:block group-hover:scale-110" />
-                    <img src="images/mobile/image-pocket-borealis.jpg" alt="" className="w-full md:hidden" />
-                    <div className="item-gradient"></div>
-                    <h5>Pocket Borealis</h5>
-                </div>
-                <div className="group item">
-                    <img src="images/desktop/image-curiosity.jpg" alt="" className="hidden w-full duration-200 md:block group-hover:scale-110" />
-                    <img src="images/mobile/image-curiosity.jpg" alt="" className="w-full md:hidden" />
-                    <div className="item-gradient"></div>
-                    <h5>The Curiosity</h5>
-                </div>
-                <div className="group item">
-                    <img src="images/desktop/image-fisheye.jpg" alt="" className="hidden w-full duration-200 md:block group-hover:scale-110" />
-                    <img src="images/mobile/image-fisheye.jpg" alt="" className="w-full md:hidden" />
-                    <div className="item-gradient"></div>
-                    <h5>Make it Fisheye</h5>
-                </div>
-            </div>
+      <div className="mt-16 container max-w-6xl px-6">
+        <div>
+          <div className="flex flex-col justify-between md:flex-row">
+            <p className="font-mono uppercase font-light text-3xl text-center">
+              Our creations
+            </p>
+            <button className="border border-black px-8 py-2 text-black bg-white hover:bg-black hover:text-white">
+              SEE ALL
+            </button>
+          </div>
         </div>
+        <div className="grid gap-6 md:grid-cols-4 mt-8">
+          <div className="group item">
+            <img
+              src="images/desktop/image-deep-earth.jpg"
+              alt=""
+              className="hidden w-full duration-200 md:block group-hover:scale-110"
+            />
+            <img
+              src="images/mobile/image-deep-earth.jpg"
+              alt=""
+              className="w-full md:hidden"
+            />
+            <div className="item-gradient"></div>
+            <h5>Deep Earth</h5>
+          </div>
+          <div className="group item">
+            <img
+              src="images/desktop/image-night-arcade.jpg"
+              alt=""
+              className="hidden w-full duration-200 md:block group-hover:scale-110"
+            />
+            <img
+              src="images/mobile/image-night-arcade.jpg"
+              alt=""
+              className="w-full md:hidden"
+            />
+            <div className="item-gradient"></div>
+            <h5>Night Arcade</h5>
+          </div>
+          <div className="group item">
+            <img
+              src="images/desktop/image-soccer-team.jpg"
+              alt=""
+              className="hidden w-full duration-200 md:block group-hover:scale-110"
+            />
+            <img
+              src="images/mobile/image-soccer-team.jpg"
+              alt=""
+              className="w-full md:hidden"
+            />
+            <div className="item-gradient"></div>
+            <h5>Soccer Team VR</h5>
+          </div>
+          <div className="group item">
+            <img
+              src="images/desktop/image-grid.jpg"
+              alt=""
+              className="hidden w-full duration-200 md:block group-hover:scale-110"
+            />
+            <img
+              src="images/mobile/image-grid.jpg"
+              alt=""
+              className="w-full md:hidden"
+            />
+            <div className="item-gradient"></div>
+            <h5>The Grid</h5>
+          </div>
+          <div className="group item">
+            <img
+              src="images/desktop/image-from-above.jpg"
+              alt=""
+              className="hidden w-full duration-200 md:block group-hover:scale-110"
+            />
+            <img
+              src="images/mobile/image-from-above.jpg"
+              alt=""
+              className="w-full md:hidden"
+            />
+            <div className="item-gradient"></div>
+            <h5>From Up Above VR</h5>
+          </div>
+          <div className="group item">
+            <img
+              src="images/desktop/image-pocket-borealis.jpg"
+              alt=""
+              className="hidden w-full duration-200 md:block group-hover:scale-110"
+            />
+            <img
+              src="images/mobile/image-pocket-borealis.jpg"
+              alt=""
+              className="w-full md:hidden"
+            />
+            <div className="item-gradient"></div>
+            <h5>Pocket Borealis</h5>
+          </div>
+          <div className="group item">
+            <img
+              src="images/desktop/image-curiosity.jpg"
+              alt=""
+              className="hidden w-full duration-200 md:block group-hover:scale-110"
+            />
+            <img
+              src="images/mobile/image-curiosity.jpg"
+              alt=""
+              className="w-full md:hidden"
+            />
+            <div className="item-gradient"></div>
+            <h5>The Curiosity</h5>
+          </div>
+          <div className="group item">
+            <img
+              src="images/desktop/image-fisheye.jpg"
+              alt=""
+              className="hidden w-full duration-200 md:block group-hover:scale-110"
+            />
+            <img
+              src="images/mobile/image-fisheye.jpg"
+              alt=""
+              className="w-full md:hidden"
+            />
+            <div className="item-gradient"></div>
+            <h5>Make it Fisheye</h5>
+          </div>
+        </div>
+      </div>
     );
 };
 
--- 8.LoopStudio/src/Footer.jsx
+++ 8.LoopStudio/src/Footer.jsx
@@ -3,14 +3,42 @@
 const Footer = () => {
   return (
     <div className="mt-16">
-        <div className="max-w-6xl bg-black">
-           <div>
-            
-           </div>
-           
+      <footer className="bg-black text-white py-8">
+        <div className="container mx-auto px-4">
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className="mb-4 md:mb-0">
+              <img src="../public/images/logo.svg" alt="Logo" />
+              <p className="text-sm text-gray-400"></p>
+            </div>
+            <nav className="flex space-x-4">
+              <a href="#">
+                <img src="../public/images/icon-instagram.svg" alt="Insta" />
+              </a>
+              <a href="#">
+                <img src="../public/images/icon-pinterest.svg" alt="Insta" />
+              </a>
+              <a href="#">
+                <img src="../public/images/icon-twitter.svg" alt="Insta" />
+              </a>
+              <a href="#">
+                <img src="../public/images/icon-facebook.svg" alt="Insta" />
+              </a>
+            </nav>
+          </div>
+
+          <nav className="flex space-x-4 mt-8 ">
+            <a href="#">About</a>
+            <a href="#">Carreres</a>
+            <a href="#">Support</a>
+            <a href="#">Contact</a>
+          </nav>
+          <div className="mt-4  text-sm text-center text-white">
+            © 2025 Your Brand. All rights reserved.
+          </div>
         </div>
+      </footer>
     </div>
-  )
+  );
 }
 
 export default Footer
